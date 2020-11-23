f0452104427c1652a0362f98b6504a781fbd3a52
Trần Đắc Dương <71543016+TranDacDuong@users.noreply.github.com>
2020-11-23 23:45:43 +0700

    Update and rename Cholesky.txt to Cholesky.py

--- Topic 2 - Solving Matrix Equations/Cholesky.py
+++ Topic 2 - Solving Matrix Equations/Cholesky.py
@@ -1,25 +1,3 @@
-Giải hệ phương trình tuyến tính bằng phương pháp Cholesky: AX=B
-
-Chạy chương trình, nhập ma trận A, B: 
-Tìm ma trận A1 đối xứng: 
-Nếu ma trận A đối xứng thì đặt A1=A
-Nếu ma trận A bất đối xứng thì A1=A^t*A và B1= A^t*B
-Khi đó phương trình trở thành A1*X=B1
-Tìm ma trận A1 ở hàm cholesky1() và tìm ma trận B1 ở hàm cholesky2()
-
-Phân tích A1 theo Cholesky: A1=S^t*S
-Tìm ra ma trận S ở hàm cholesky(), đồng thời kiểm tra định thức ma trận A1 có bằng 0 không? Nếu det(A1)=0 thì kết thúc chương trình
-Khi đó phương trình trở thành: S^t*S*X=B1
-
-Đặt Y=S*X có:  S^t*Y=B1
-Giải phương trình này tìm ma trận Y bằng hàm cholesky3()
-
-Giải phương trình SX=Y ta được nghiệm X của phương trình bằng hàm cholesky4()
-
-
-[[[[[[[[[[CODE]]]]]]]]]]]]]]]
-
-
 from cmath import sqrt
 import pprint
 import scipy
