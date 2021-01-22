a97218fa0196d219af8a36b9cbdc8c7664571ffd
Do Manh Dung?? <62107278+curiosity29@users.noreply.github.com>
2021-01-22 18:34:18 +0700

    Euler, add plot for R-K

--- Topic 5 - Solving Differential Equations/28.Runge_Kutta/main_rungekutta.py
+++ Topic 5 - Solving Differential Equations/28.Runge_Kutta/main_rungekutta.py
@@ -5,7 +5,17 @@ import sys
 
 from lib_rungekutta import *
 
-uu = rungekutta_oop("y - x*x + 1", 0, 0.5, 0.2, 10, 4);
+uu = rungekutta_oop("y - x*x + 1", 0, 0.5, 0.01, 10, 4);
 g = (uu.Solve());
 
 for x in g: print(x);
+
+
+import matplotlib.pyplot as plt
+def PlotPairs(pairList):
+    t,x = zip(*pairList)
+    plt.scatter(t,x)
+    plt.show()
+
+
+PlotPairs(g)
