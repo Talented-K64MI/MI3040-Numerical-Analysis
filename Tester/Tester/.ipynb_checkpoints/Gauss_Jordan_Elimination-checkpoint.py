cf38ea7e6dc834b19e7cffef8b867a02ba472eae
bu1th4nh <thanh10973@gmail.com>
2021-01-26 21:32:26 +0700

    Cleanup

--- Tester/Tester/.ipynb_checkpoints/Gauss_Jordan_Elimination-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from GaussJordan_source import Gauss_Jordan_Algorithms
-
-try:
-    RUN = Gauss_Jordan_Algorithms()
-    RUN.main()
-except:
-    f = open("GJ_output.txt", "w")
-    f.write("Da co loi xay ra!")
-    f.close()
